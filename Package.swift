7ed244b6ad4024e135eb23f87040fa9bbb0515e5
Kristiyan Dobrev <dobrevkristiyan@gmail.com>
2020-09-30 11:45:12 +0200

    Bumped minimum iOS version to 9.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Nantes",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "Nantes", targets: ["Nantes"])
     ],
